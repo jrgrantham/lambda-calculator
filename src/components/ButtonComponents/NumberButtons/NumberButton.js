ec4fd5a26f6e6c434c56f5499767948e003d3171
James Grantham <james.r.grantham@gmail.com>
2019-09-10 14:58:10 +0100

    Numbers displaying

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,8 @@ const NumberButton = (props) => {
   const { number } = props;
   return (
     <>
-      <button>{number}</button>{/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{number}</button>
+      {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
