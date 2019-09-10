1ff81612fb7c436cb910774905c0482d2acf99e4
James Grantham <james.r.grantham@gmail.com>
2019-09-10 21:34:29 +0100

    useState added,

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const NumberButton = (props) => {
-  const { number } = props;
+  const { number, nextButton } = props;
   return (
     <>
-      <button>{number}</button>
+      <button onClick={evt => nextButton(number)}>{number}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
