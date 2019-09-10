251cd4193df95e9707e7763a29349ee75dbba461
James Grantham <james.r.grantham@gmail.com>
2019-09-10 16:48:07 +0100

    Specials and Display displaying

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 // Don't forget to import any extra css/scss files you build into the correct component
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
+import Display from "./components/DisplayComponents/Display"
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 import {numbers, operators, specials} from "./data";
@@ -22,6 +24,8 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers numbers={numbers}/>
         <Operators operators={operators}/>
+        <Specials specials={specials}/>
+        <Display/>
       </div>
     </div>
   );
